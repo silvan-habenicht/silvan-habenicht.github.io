50087546c099111620b8f33f48d0b3c651d8fc42
Silvan Habenicht <silvan.habenicht@web.de>
2019-03-19 14:12:19 +0100

    viele verbesserungen

---
+++ check-test.js
@@ -0,0 +1,175 @@
+/*eslint-env browser*/
+var commandSolved = false,
+    factorySolved = false,
+    decoratorSolved = false,
+    strategySolved = false,
+    observerSolved = false;
+
+function checkSolved() {
+    "use strict";
+    if (!commandSolved) { return; }
+    if (!factorySolved) { return; }
+    if (!decoratorSolved) { return; }
+    if (!strategySolved) { return; }
+    if (!observerSolved) { return; }
+    document.getElementById("match").play();
+    document.getElementById("certificate").disabled = false;
+}
+
+/* Checking the multiple choice test for the command pattern. */
+function checkCommand(pattern) { /* exported checkCommand */
+    "use strict";
+    var patternForm = document.getElementById(pattern + "Form"),
+        patternMenu = document.getElementById(pattern + "Menu"),
+        patternResult = document.getElementById(pattern + "Result");
+    
+    if (!patternForm.commandA1.checked &&
+            !patternForm.commandA2.checked &&
+            patternForm.commandA3.checked &&
+            !patternForm.commandA4.checked &&
+            !patternForm.commandB1.checked &&
+            !patternForm.commandB2.checked &&
+            !patternForm.commandB3.checked &&
+            patternForm.commandB4.checked) {
+        
+        patternMenu.style.color = "rgba(0,140,69,1)";
+        patternMenu.innerHTML = "Command &#10003;";
+        patternResult.style.color = "rgba(0,140,69,1)";
+        patternResult.innerHTML = "&#10003;";
+        commandSolved = true;
+        document.getElementById("bling").play();
+        checkSolved();
+    
+    } else {
+        patternMenu.style.color = "darkred";
+        patternMenu.innerHTML = "Command";
+        patternResult.style.color = "darkred";
+        patternResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
+        commandSolved = false;
+    }
+}
+
+/* Checking the multiple choice test for the factory pattern. */
+function checkFactory(pattern) { /* exported checkFactory */
+    "use strict";
+    var patternForm = document.getElementById(pattern + "Form"),
+        patternMenu = document.getElementById(pattern + "Menu"),
+        patternResult = document.getElementById(pattern + "Result");
+    
+    if (!patternForm.factoryA1.checked &&
+            patternForm.factoryA2.checked &&
+            !patternForm.factoryA3.checked &&
+            !patternForm.factoryA4.checked &&
+            !patternForm.factoryB1.checked &&
+            patternForm.factoryB2.checked &&
+            patternForm.factoryB3.checked &&
+            !patternForm.factoryB4.checked) {
+        
+        patternMenu.style.color = "rgba(0,140,69,1)";
+        patternMenu.innerHTML = "Factory &#10003;";
+        patternResult.style.color = "rgba(0,140,69,1)";
+        patternResult.innerHTML = "&#10003;";
+        factorySolved = true;
+        checkSolved();
+    
+    } else {
+        patternMenu.style.color = "darkred";
+        patternMenu.innerHTML = "Factory";
+        patternResult.style.color = "darkred";
+        patternResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
+        factorySolved = false;
+    }
+}
+
+/* Checking the multiple choice test for the decorator pattern. */
+function checkDecorator(pattern) { /* exported checkDecorator */
+    "use strict";
+    var patternForm = document.getElementById(pattern + "Form"),
+        patternMenu = document.getElementById(pattern + "Menu"),
+        patternResult = document.getElementById(pattern + "Result");
+    
+    if (patternForm.decoratorA1.checked &&
+            !patternForm.decoratorA2.checked &&
+            !patternForm.decoratorA3.checked &&
+            !patternForm.decoratorA4.checked) {
+        
+        patternMenu.style.color = "rgba(0,140,69,1)";
+        patternMenu.innerHTML = "Decorator &#10003;";
+        patternResult.style.color = "rgba(0,140,69,1)";
+        patternResult.innerHTML = "&#10003;";
+        decoratorSolved = true;
+        checkSolved();
+    
+    } else {
+        patternMenu.style.color = "darkred";
+        patternMenu.innerHTML = "Decorator";
+        patternResult.style.color = "darkred";
+        patternResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
+        decoratorSolved = false;
+    }
+}
+
+/* Checking the multiple choice test for the strategy pattern. */
+function checkStrategy(pattern) { /* exported checkStrategy */
+    "use strict";
+    var patternForm = document.getElementById(pattern + "Form"),
+        patternMenu = document.getElementById(pattern + "Menu"),
+        patternResult = document.getElementById(pattern + "Result");
+    
+    if (!patternForm.strategyA1.checked &&
+            !patternForm.strategyA2.checked &&
+            !patternForm.strategyA3.checked &&
+            patternForm.strategyA4.checked) {
+        
+        patternMenu.style.color = "rgba(0,140,69,1)";
+        patternMenu.innerHTML = "Strategy &#10003;";
+        patternResult.style.color = "rgba(0,140,69,1)";
+        patternResult.innerHTML = "&#10003;";
+        strategySolved = true;
+        checkSolved();
+    
+    } else {
+        patternMenu.style.color = "darkred";
+        patternMenu.innerHTML = "Strategy";
+        patternResult.style.color = "darkred";
+        patternResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
+        strategySolved = false;
+    }
+}
+
+/* Checking the multiple choice test for the observer pattern. */
+function checkObserver(pattern) { /* exported checkObserver */
+    "use strict";
+    var patternForm = document.getElementById(pattern + "Form"),
+        patternMenu = document.getElementById(pattern + "Menu"),
+        patternResult = document.getElementById(pattern + "Result");
+    
+    if (!patternForm.observerA1.checked &&
+            !patternForm.observerA2.checked &&
+            !patternForm.observerA3.checked &&
+            patternForm.observerA4.checked &&
+            !patternForm.observerB1.checked &&
+            !patternForm.observerB2.checked &&
+            patternForm.observerB3.checked &&
+            !patternForm.observerB4.checked) {
+        
+        patternMenu.style.color = "rgba(0,140,69,1)";
+        patternMenu.innerHTML = "Observer &#10003;";
+        patternResult.style.color = "rgba(0,140,69,1)";
+        patternResult.innerHTML = "&#10003;";
+        observerSolved = true;
+        checkSolved();
+    
+    } else {
+        patternMenu.style.color = "darkred";
+        patternMenu.innerHTML = "Observer";
+        patternResult.style.color = "darkred";
+        patternResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
+        observerSolved = false;
+    }
+}
+
+function getCertificate() { /* exported getCertificate*/
+    "use strict";
+    window.open("pictures/pasta.jpg");
+}
--- script.js
+++ script.js
@@ -9,15 +9,18 @@ function openCloseMenu() {
     if (!opened) {
         opened = 1;
         document.getElementById("patternMenu").style.width = "240px";
-        if (window.innerWidth >= 1280) {
-            document.getElementById("patternMenu").style.width = (window.innerWidth - 802) / 2 + "px";
-        } else if (window.innerWidth > 600) {
-            document.getElementById("main").style.marginLeft = "240px";
-        }
     } else {
         opened = 0;
         document.getElementById("patternMenu").style.width = "0";
-        document.getElementById("main").style.marginLeft = "auto";
+    }
+    window.onresize();
+}
+
+/* exported conditionCloseMenu to index.html*/
+function conditionCloseMenu() {
+    "use strict";
+    if (window.innerWidth < 1040) {
+        openCloseMenu();
     }
 }
 
@@ -25,65 +28,50 @@ function openCloseMenu() {
 /* call function for syntax highlighting from highlight.pack.js*/
 hljs.initHighlightingOnLoad();
 
-if (window.innerWidth > 600) {
-    if (window.innerWidth < 1280) {
-        document.getElementById("main").style.marginLeft = "240px";
+if (window.innerWidth > 1040) {
+    if (window.innerWidth > 1280) {
+        document.getElementById("main").style.marginLeft = "auto";
+    } else {
+        document.getElementById("main").style.marginLeft = (window.innerWidth - 800) / 2 + "px";
     }
     setTimeout(function () {
         "use strict";
         openCloseMenu();
-    }, 500);
+    }, 1000);
 }
 
-window.onresize = function (event) {
+window.onresize = function (event) { // eslint-disable-line no-unused-vars
     "use strict";
     
     if (opened) {
-        if (window.innerWidth < 800) {
+        if (window.innerWidth < 560) {
+            document.getElementById("main").style.padding = "2em 1em 2em 4em";
+            document.getElementById("main").style.marginLeft = "0%";
+            document.getElementById("patternMenu").style.width = "240px";
+        } else if (window.innerWidth < 800) {
             document.getElementById("main").style.padding = "2em 1em 2em 1em";
+            document.getElementById("main").style.marginLeft = "240px";
+            document.getElementById("patternMenu").style.width = "240px";
+        } else if (window.innerWidth < 1280) {
+            document.getElementById("main").style.padding = "2em 4em 4em";
+            document.getElementById("main").style.marginLeft = "240px";
+            document.getElementById("patternMenu").style.width = "240px";
         } else {
             document.getElementById("main").style.padding = "2em 4em 4em";
-            if (window.innerWidth >= 1280) {
-                document.getElementById("main").style.marginLeft = "auto";
-                document.getElementById("patternMenu").style.width = (window.innerWidth - 802) / 2 + "px";
-            } else if (window.innerWidth > 600) {
-                document.getElementById("main").style.marginLeft = "240px";
-            }
+            document.getElementById("main").style.marginLeft = "auto";
+            document.getElementById("patternMenu").style.width = (window.innerWidth - 802) / 2 + "px";
         }
     } else {
-        if (window.innerWidth < 600) {
+        if (window.innerWidth < 560) {
             document.getElementById("main").style.padding = "2em 1em 2em 4em";
+            document.getElementById("main").style.marginLeft = "0%";
+        } else if (window.innerWidth < 800) {
+            document.getElementById("main").style.padding = "2em 4em 4em";
+            document.getElementById("main").style.marginLeft = "0%";
         } else {
             document.getElementById("main").style.padding = "2em 4em 4em";
+            document.getElementById("main").style.marginLeft = (window.innerWidth - 800) / 2 + "px";
         }
     }
 };
 
-/* exported checkCommand to index.html*/
-function checkCommand() {
-    "use strict";
-    var commandForm = document.getElementById("commandForm"),
-        commandMenu = document.getElementById("commandMenu"),
-        commandResult = document.getElementById("commandResult");
-    
-    if (!commandForm.commandA1.checked &&
-            !commandForm.commandA2.checked &&
-            commandForm.commandA3.checked &&
-            !commandForm.commandA4.checked &&
-            !commandForm.commandB1.checked &&
-            !commandForm.commandB2.checked &&
-            !commandForm.commandB3.checked &&
-            commandForm.commandB4.checked) {
-        
-        commandMenu.style.color = "rgba(0,140,69,1)";
-        commandMenu.innerHTML = "Command &#10003;";
-        commandResult.style.color = "rgba(0,140,69,1)";
-        commandResult.innerHTML = "&#10003;";
-    
-    } else {
-        commandMenu.style.color = "darkred";
-        commandMenu.innerHTML = "Command";
-        commandResult.style.color = "darkred";
-        commandResult.innerHTML = "&#10007; Bitte überprüfe deine Eingaben.";
-    }
-}
